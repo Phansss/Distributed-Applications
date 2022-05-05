79cf376dbb2852c7ee54cb51ab53626381dba929
Daan Dekeyser <daan.dekeyser1@student.kuleuven.be>
2022-05-05 14:43:07 +0200

    Test 2

--- src/main/java/servlets/MyHelloServlet.java
+++ src/main/java/servlets/MyHelloServlet.java
@@ -24,6 +24,7 @@ public class MyHelloServlet extends HttpServlet {
         writer.println("<h1>Hello</h1>");
         // Test
         writer.println("Test");
+        writer.println("Test 2");
         writer.println("<p>The result of 15 + 32 = " + bean.doSomethingReallyDifficult(15,32) + ".</p>");
         writer.println("</body>\n</html>");
 

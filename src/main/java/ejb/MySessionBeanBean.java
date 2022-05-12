78bc5a7d95f9a5623d9f23b51585edb9d5a2ea42
phanss <36855110+Phansss@users.noreply.github.com>
2022-05-12 16:20:18 +0200

    Working JPA

--- src/main/java/ejb/MySessionBeanBean.java
+++ src/main/java/ejb/MySessionBeanBean.java
@@ -1,11 +1,28 @@
 package ejb;
 
+import entities.PersonEntity;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import jakarta.persistence.Query;
+
+import java.util.List;
+
 @jakarta.ejb.Stateless(name = "MySessionBeanEJB")
 public class MySessionBeanBean {
+
+@PersistenceContext(unitName = "DADemoPU")
+    EntityManager em;
+
     public MySessionBeanBean() {
     }
 
     public int doSomethingReallyDifficult(int a, int b) {
         return  a + b;
+    }
+
+    public List<PersonEntity> findPersonByName(String name) {
+        Query query = em.createQuery("SELECT p FROM PersonEntity p WHERE p.name = :searchname", PersonEntity.class);
+        query.setParameter("searchname", "%" + name + "%");
+        return query.getResultList();
     }
 }
